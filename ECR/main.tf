87dd8f38ca7065e7a048b5e6e87fd47cbdbf9e52
hodaifa <150153831+hodaifa-ech@users.noreply.github.com>
2024-12-29 22:09:07 +0100

    add infra for  ecr repo

---
+++ ECR/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
+resource "aws_ecrpublic_repository" "user-service" {
+  provider         = aws.us_east_1
+  repository_name  = "user-service"
+
+  catalog_data {
+    about_text        = "This repository contains Docker images for the user-service project."
+    architectures     = ["ARM"]
+    description       = "Public repository for user-service project."
+  
+    operating_systems = ["Linux"]
+    usage_text        = "Use these images for user-service-related applications."
+  }
+
+  tags = {
+    env = "production"
+    app = "user-service"
+  }
+}
+
+output "repository_uri" {
+  value = aws_ecrpublic_repository.user-service.repository_uri
+}
